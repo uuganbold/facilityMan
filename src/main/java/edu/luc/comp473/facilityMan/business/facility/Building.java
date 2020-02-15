8d079f96caa025ea41b01844b9db98de9bacc772
uuganbold <uuganhuu@gmail.com>
2020-02-15 15:06:26 -0600

    added some classes

---
+++ src/main/java/edu/luc/comp473/facilityMan/business/facility/Building.java
@@ -0,0 +1,13 @@
+package edu.luc.comp473.facilityMan.business.facility;
+
+import java.util.List;
+
+public class Building extends Facility {
+    private List<Unit> units;
+    private LocationManager locationManager;
+
+    public int getCapacity() {
+        // TODO calculate capacity here
+        return 0;
+    }
+}
--- src/main/java/edu/luc/comp473/facilityMan/business/facility/Facility.java
+++ src/main/java/edu/luc/comp473/facilityMan/business/facility/Facility.java
@@ -1,5 +1,13 @@
 package edu.luc.comp473.facilityMan.business.facility;
 
-public class Facility {
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+public abstract class Facility {
+    protected Long id;
+    protected FacilityDetail detail;
+
+    public abstract int getCapacity();
 }
