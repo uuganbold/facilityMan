86a94d3c209de329c58429e91d23a1240cc07820
uuganbold <uuganhuu@gmail.com>
2020-02-15 14:36:59 -0600

    initial commit spring boot

---
+++ src/main/java/edu/luc/comp473/facilityMan/FacilityManApplication.java
@@ -0,0 +1,13 @@
+package edu.luc.comp473.facilityMan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacilityManApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacilityManApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/luc/comp473/facilityMan/FacilityManApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.luc.comp473.facilityMan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacilityManApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
