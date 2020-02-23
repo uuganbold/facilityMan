27038a058e997ec688dc02059640990daf695543
uuganbold <uuganhuu@gmail.com>
2020-02-23 10:57:10 -0600

    created spring boot command line runner

--- src/main/java/edu/luc/comp473/facilityMan/FacilityManApplication.java
+++ src/main/java/edu/luc/comp473/facilityMan/FacilityManApplication.java
@@ -1,13 +1,19 @@
 package edu.luc.comp473.facilityMan;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FacilityManApplication {
+public class FacilityManApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FacilityManApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello");
 	}
 
 }
